476691a2968a25e94c78d8ad4befeafe45787521
Fiodor Tarasenko <tarasenko.fk@phystech.edu>
2023-11-15 17:52:38 +0300

    main cleared

--- graph.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-data = np.loadtxt('/home/b03-301/Desktop/maratfedornasta/datamaratpokoy.txt')
-data2 = np.loadtxt('/home/b03-301/Desktop/maratfedornasta/timepokoymaratalone.txt')
-time_start = data2[0]
-time_end = data2[1]
-data3 = np.linspace(time_start, time_end, len(data))
-plt.plot(data3, data)
-plt.show()
