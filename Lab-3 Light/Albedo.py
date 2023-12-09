a4a21dd0f2bb0bb8b0233bbe19f541d00130e00c
FiodorTarasenko <145488684+FiodorTarasenko@users.noreply.github.com>
2023-12-09 22:55:57 +0300

    Графики и функции интенсивности

---
+++ Lab-3 Light/Albedo.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import imageio.v2 as imageio
+from cycler import cycler
+
+o = []
+with open(r'coefficients.txt') as f:
+    for line in f:
+        o.append(float(line))
+k = o[0]
+c = o[1]
+
+def calib(x, pos):
+    return int(x * k + c)
+def readIntensity(photoName):
+    photo = imageio.imread(photoName)
+    background = photo[896:1111, 664:1093, 0:3]
+
+    cut = photo[896:1111, 664:1093, 0:3]
+    rgb = np.mean(cut, axis=0)
+    luma = 0.2989 * rgb[:, 0] + 0.5866 * rgb[:, 1] + 0.1144 * rgb[:, 2]
+
+    plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b'])))
+    return luma
+
+fig = plt.figure(figsize=(10, 5), dpi=200)
+ax = fig.add_subplot()
+
+plt.title('Значение альбедо\nдля различных цветов поверхностей')
+plt.xlabel('Длина волны [нм]')
+plt.ylabel('Альбедо')
+plt.minorticks_on()
+def albedo(i1, i2):
+    return np.delete(readIntensity(i1)/readIntensity(i2), [-1, 0, 1, 2, 3, 4])
+print(albedo('green_spec.jpg', 'white_spec.jpg'))
+
+plt.plot(albedo('white_spec.jpg', 'white_spec.jpg'), 'black', label='I')
+plt.plot((albedo('yellow_spec.jpg', 'white_spec.jpg')), 'y', label='y')
+plt.plot((albedo('red_spec.jpg', 'white_spec.jpg')), 'r', label='r')
+plt.plot((albedo('green_spec.jpg', 'white_spec.jpg')), 'g', label='g')
+plt.plot((albedo('blue_spec.jpg', 'white_spec.jpg')), 'b', label='b')
+plt.legend()
+start, end = ax.get_xlim()
+ax.xaxis.set_ticks(((np.arange(((start * k + c) // 50 + 1) * 50, end * k + c, 50, dtype=int)) - c) / k)
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(calib))
+
+plt.savefig('Albedo.png')
+plt.show()
